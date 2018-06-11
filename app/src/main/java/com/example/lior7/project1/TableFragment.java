49308deae6d8e3feee849a3fa99efbb32108c9ca
lior7704 <37957751+lior7704@users.noreply.github.com>
2018-06-11 14:49:19 +0300

    Added fragments(non functional) part 2

---
+++ app/src/main/java/com/example/lior7/project1/TableFragment.java
@@ -0,0 +1,16 @@
+package com.example.lior7.project1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TableFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.table_view, container, false);
+    }
+}
