d685be37a483f9f0829713b6efa68c1a2e8cf886
lior7704 <37957751+lior7704@users.noreply.github.com>
2018-06-11 14:47:00 +0300

    Added fragments(non functional)

--- app/src/main/java/com/example/lior7/project1/DifficultyActivity.java
+++ app/src/main/java/com/example/lior7/project1/DifficultyActivity.java
@@ -1,14 +1,16 @@
 package com.example.lior7.project1;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class DifficultyActivity extends AppCompatActivity implements View.OnClickListener {
+public class DifficultyActivity extends FragmentActivity implements View.OnClickListener {
     public static final String NUM_OF_CUBES = "NUM_OF_CUBES";
     public static final String TIME = "TIME";
     public static final int NUM_CARDS_EASY = 2, EASY_TIME = 30;
--- app/src/main/java/com/example/lior7/project1/MainActivity.java
+++ app/src/main/java/com/example/lior7/project1/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.lior7.project1;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity{
     public static final String NAME = "NAME";
     public static final String AGE = "AGE";
-    private EditText editTextName, editTextAge;
-    private Button btnStart;
     private boolean isValidInputs;
+    private EditText editTextName, editTextAge;
+    private Button StartButton;
+    private Button mapButton;
+    private Button tableButton;
+    Fragment tableFragment;
+    Fragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,29 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         bindUI();
-        btnStart.setOnClickListener(new View.OnClickListener() {
+
+        // Create and set Android Fragment as default.
+        tableFragment = new TableFragment();
+        this.setDefaultFragment(tableFragment);
+
+        // Click this button to display table fragment.
+        tableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(tableFragment);
+            }
+        });
+
+        // Click this button to display map fragment.
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mapFragment = new MapFragment();
+                replaceFragment(mapFragment);
+            }
+        });
+
+        StartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean isContinue = checkInputs();
@@ -34,7 +64,9 @@ public class MainActivity extends AppCompatActivity{
         editTextName = findViewById(R.id.editTextName);
         editTextName.requestFocus();
         editTextAge = findViewById(R.id.editTextAge);
-        btnStart = findViewById(R.id.buttonStart);
+        StartButton = findViewById(R.id.buttonStart);
+        tableButton = findViewById(R.id.dynamic_fragment_table_button);
+        mapButton = findViewById(R.id.dynamic_fragment_map_button);
     }
 
     private boolean checkInputs()
@@ -60,5 +92,27 @@ public class MainActivity extends AppCompatActivity{
         intent.putExtra(NAME, name);
         intent.putExtra(AGE, age);
         startActivity(intent);
+    }
+
+    // This method is used to set the default fragment that will be shown.
+    private void setDefaultFragment(Fragment defaultFragment)
+    {
+        this.replaceFragment(defaultFragment);
+    }
+
+    // Replace current Fragment with the destination Fragment.
+    public void replaceFragment(Fragment destFragment)
+    {
+        // First get FragmentManager object.
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+
+        // Begin Fragment transaction.
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the layout holder with the required Fragment object.
+        fragmentTransaction.replace(R.id.fragment_container, destFragment);
+
+        // Commit the Fragment replace action.
+        fragmentTransaction.commit();
     }
 }
